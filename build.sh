907b99b9b5cb50ca846056065da9bff826fb5a75
abc <abc@abc.abc>
2020-10-26 19:33:07 +0100

    buildscript remove + DOCKERFILE.bundle

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker build ./ -f DOCKERFILE.build --tag raspi-website-build
-docker run -v /wrk:/wrk -it raspi-website-build
+#docker build ./ -f DOCKERFILE.build --tag raspi-website-builder
+#docker run -v /wrk:/wrk -it raspi-website-builder
+#docker build ./ -f DOCKERFILE.bundle --tag raspi-website ### check .dockerignore
+#docker push 
